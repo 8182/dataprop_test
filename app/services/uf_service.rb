40c10b3574a5965468043f55189b8a4920267653
brayan <brayan.valdebenito@dominospizza.cl>
2025-09-17 15:50:53 -0300

    added base url for path

--- app/services/uf_service.rb
+++ app/services/uf_service.rb
@@ -7,6 +7,7 @@ class UfService
   def self.fetch(year: nil, month: nil, day: nil, flow: nil, fill_db_with_search: nil)
 
     #vemos los argumentos que se ingresaron, y los vamos metiendo al path, en caso de venir
+    path = BASE_URL.dup # copiamos el valor de la constante, ya que si la asignamos directamente no se podra modificar
     path += "/#{year}" if year
     path += "/#{month.to_s.rjust(2, '0')}" if month #usamos rjust para rellenar a la izquierda con 0 en caso de que se de por ejemplo el value de 1, pasaria a ser 01
     path += "/dias/#{day.to_s.rjust(2, '0')}" if day
@@ -15,6 +16,8 @@ class UfService
     url = URI("#{path}?apikey=#{API_KEY}&formato=json")
     response = Net::HTTP.get(url)
     data = JSON.parse(response)
-    
 
+    if fill_db_with_search
+      # si era true, guardamos en db
+    end
 end
