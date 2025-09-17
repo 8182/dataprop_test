05c8bcb54ada0ba2b042daec7d71163dc3cd8b0f
brayan <brayan.valdebenito@dominospizza.cl>
2025-09-17 15:15:51 -0300

    fix name for fields

--- app/models/uf_value.rb
+++ app/models/uf_value.rb
@@ -1,10 +1,6 @@
 class UfValue < ApplicationRecord
-  validates :date, presence: true, uniqueness: true
-  validates :value, presence: true, numericality: { greater_than: 0 }
-
-
-
+  validates :uf_date, presence: true, uniqueness: true
+  validates :uf_value, presence: true, numericality: { greater_than: 0 }
 
 
 end
-  
