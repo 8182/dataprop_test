0f5930474af20aaa6ab3bd61137225c28c67f56c
brayan <brayan.valdebenito@dominospizza.cl>
2025-09-18 18:17:05 -0300

    fix for deploy

--- config/application.rb
+++ config/application.rb
@@ -10,16 +10,16 @@ module DatapropTest
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.0
-    # not contain `.rb` files, or that should not be reloaded or eager loaded.
-    # Common ones are `templates`, `generators`, or `middleware`, for example.
-  config.autoload_lib(ignore: %w[assets tasks])
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    # Desactivar Action Cable completamente
+    config.action_cable.mount_path = nil
+    config.action_cable.url = nil
+    config.action_cable.allowed_request_origins = nil
+    config.action_cable.disable_request_forgery_protection = true
+
+    config.cache_store = :memory_store
+
+    config.autoload_lib(ignore: %w[assets tasks])
+
   end
 end
