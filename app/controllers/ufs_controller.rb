7e4f5ffe1cd51714299e48ba7ca54c33ba0a1a64
brayan <brayan.valdebenito@dominospizza.cl>
2025-09-17 22:12:35 -0300

    added controller and routes for form view and controller for process fetch and return results in json

--- app/controllers/ufs_controller.rb
+++ app/controllers/ufs_controller.rb
@@ -19,8 +19,37 @@ class UfsController < ApplicationController
     end
   end
 
+  #vista con el form para buscar fechas 
+  def form_by_date
+    today = Date.today
 
-  def show_by_date #metodo pensando para retornar en json y ser usado con fetch de js, y actualizar dinamicamente desde la vista
+    # cache para valor de hoy
+    expires_in_today = (Time.current.end_of_day - Time.current).to_i
+
+    #generamos un cache con mismo funcionamiento que en el index
+    @uf_today = Rails.cache.fetch("uf_#{today}", expires_in: expires_in_today) do
+      data = UfService.fetch(year: today.year, month: today.month, day: today.day, fill_db_with_search: false)
+      uf = data && data["UFs"]&.first
+      uf ? { fecha: uf["Fecha"], valor: uf["Valor"].to_s.gsub('.', '').gsub(',', '.').to_f } : nil
+    end
+
+    # como fallback, si la api responde nil para el dia, se tomara el valor del dia anterior, y se creara la variable @var con valor, para mostrar en el front que hubo este cambio 
+    if @uf_today.nil?
+      yesterday = today - 1.day
+      @uf_today = Rails.cache.fetch("uf_#{yesterday}", expires_in: 1.year) do
+        data = UfService.fetch(year: yesterday.year, month: yesterday.month, day: yesterday.day, fill_db_with_search: false)
+        uf = data && data["UFs"]&.first
+        uf ? { fecha: uf["Fecha"], valor: uf["Valor"].to_s.gsub('.', '').gsub(',', '.').to_f } : nil
+      end
+      @var = "Mostrando UF del día anterior (#{yesterday})"
+    else
+      @var = nil
+    end
+
+  end
+
+  # metodo que se usara con fetch desde el front de form_by_date
+  def search
 
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root "ufs#index" #se deja como root el index, que cargara en el controlador 1 año de valores de la uf
 
   get "/ufs", to: "ufs#index", as: :ufs
-  get "/ufs/fecha/:fecha", to: "ufs#show_by_date", as: :by_fecha_ufs
+  get '/ufs/search', to: 'ufs#search'
+  get '/ufs/busqueda', to: 'ufs#form_by_date'
 end
